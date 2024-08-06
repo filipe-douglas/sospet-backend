623dd4a21d35ac3074b3d4e9188e11eaaed096cc
Filipe Douglas <felipe.douglas.inf1@gmail.com>
2024-08-06 16:17:33 -0300

    config: disable CSRF and set session management to stateless

--- src/main/java/br/com/umbrella/sospet/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/umbrella/sospet/infra/security/SecurityConfigurations.java
@@ -2,7 +2,7 @@ package br.com.umbrella.sospet.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+// import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
